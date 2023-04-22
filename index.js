d65e0baf0aefd702d88060dadf6ecd683fb7d9b0
Moliver42j <matthewoliver88@gmail.com>
2023-04-22 17:37:19 +0100

    modified to accept payload action

--- index.js
+++ index.js
@@ -1,63 +1,22 @@
-const { EC2, SSM } = require('@aws-sdk/client-ec2');
-const { SSMClient, SendCommandCommand } = require('@aws-sdk/client-ssm');
+const AWS = require('aws-sdk');
+const ec2 = new AWS.EC2({ region: 'your-region' });
 
-exports.handler = async (event) => {
-  // Set the region where your EC2 instance is located
-  const region = 'eu-west-1';
-
-  // Create an EC2 client instance
-  const ec2Client = new EC2({ region });
-
-  // Set the name of the EC2 instance you want to start
-  const instanceName = 'zomboidServerForTheBoys';
+exports.handler = async (event, context) => {
+  const instanceId = 'i-0795bc283c713e30d';
 
   try {
-    // Retrieve the instance ID for the specified instance name
-    const data = await ec2Client.describeInstances({
-      Filters: [
-        {
-          Name: 'tag:Name',
-          Values: [instanceName],
-        },
-      ],
-    });
-
-    const instanceId = data.Reservations[0].Instances[0].InstanceId;
-    console.log(`Instance ID: ${instanceId}`);
-
-    // Set the command you want to run on the EC2 instance
-    const command = 'ls -l';
-
-    // Start the EC2 instance
-    await ec2Client.startInstances({ InstanceIds: [instanceId] });
-    console.log(`Instance started: ${instanceId}`);
-
-    // Wait for the EC2 instance to start
-    await ec2Client.waitFor('instanceRunning', { InstanceIds: [instanceId] });
-    console.log(`Instance running: ${instanceId}`);
-
-    // Send the command to the EC2 instance using SSM
-    const ssmClient = new SSMClient({ region });
-    const data2 = await ssmClient.send(new SendCommandCommand({
-      DocumentName: 'AWS-RunShellScript',
-      InstanceIds: [instanceId],
-      Parameters: { commands: [command] },
-    }));
-
-    console.log(`Command sent to instance: ${instanceId}`);
-    console.log(`Command ID: ${data2.Command.CommandId}`);
-    
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify(`Command sent to instance: ${instanceId}\nCommand ID: ${data2.Command.CommandId}`),
-    };
-    return response;
-  } catch (err) {
-    console.log(`Error: ${err}`);
-    const response = {
-      statusCode: 500,
-      body: JSON.stringify(`Error: ${err}`),
-    };
-    return response;
+    if (event.action === 'stop') {
+      await ec2.stopInstances({ InstanceIds: [instanceId] }).promise();
+      console.log(`Instance ${instanceId} stopped`);
+    } else if (event.action === 'start') {
+      await ec2.startInstances({ InstanceIds: [instanceId] }).promise();
+      console.log(`Instance ${instanceId} started`);
+    } else {
+      console.log(`Invalid action ${event.action}`);
+      return;
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
   }
 };
