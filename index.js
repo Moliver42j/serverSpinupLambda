354937c9026c64530dabac5554350752a21f3a7b
Moliver42j <matthewoliver88@gmail.com>
2023-04-22 17:40:38 +0100

    changes to stop duplicate commands

--- index.js
+++ index.js
@@ -5,6 +5,20 @@ exports.handler = async (event, context) => {
   const instanceId = 'i-0795bc283c713e30d';
 
   try {
+    const data = await ec2.describeInstances({ InstanceIds: [instanceId] }).promise();
+    const instance = data.Reservations[0].Instances[0];
+    const state = instance.State.Name;
+
+    if (event.action === 'start' && state === 'running') {
+      console.log(`Instance ${instanceId} is already running`);
+      return;
+    }
+
+    if (event.action === 'stop' && state === 'stopped') {
+      console.log(`Instance ${instanceId} is already stopped`);
+      return;
+    }
+
     if (event.action === 'stop') {
       await ec2.stopInstances({ InstanceIds: [instanceId] }).promise();
       console.log(`Instance ${instanceId} stopped`);
