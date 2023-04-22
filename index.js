0fc7fad69bb6a034487e6e5f2583ef3f60fe8a43
Moliver42j <matthewoliver88@gmail.com>
2023-04-22 16:10:39 +0100

    moved to aws sdk v3

--- index.js
+++ index.js
@@ -1,34 +1,63 @@
-const AWS = require('aws-sdk');
-const ec2 = new AWS.EC2({ region: 'your-ec2-instance-region' });
+const { EC2, SSM } = require('@aws-sdk/client-ec2');
+const { SSMClient, SendCommandCommand } = require('@aws-sdk/client-ssm');
 
-exports.handler = async (event, context) => {
+exports.handler = async (event) => {
+  // Set the region where your EC2 instance is located
+  const region = 'eu-west-1';
+
+  // Create an EC2 client instance
+  const ec2Client = new EC2({ region });
+
+  // Set the name of the EC2 instance you want to start
+  const instanceName = 'zomboidServerForTheBoys';
+
   try {
-    const instanceParams = {
-      ImageId: 'your-ec2-instance-image-id',
-      InstanceType: 'your-ec2-instance-type',
-      MinCount: 1,
-      MaxCount: 1,
-      KeyName: 'your-ec2-instance-key-name',
-      SecurityGroupIds: ['your-ec2-instance-security-group-id'],
-      UserData: 'your-user-data-script', // This is optional
-    };
+    // Retrieve the instance ID for the specified instance name
+    const data = await ec2Client.describeInstances({
+      Filters: [
+        {
+          Name: 'tag:Name',
+          Values: [instanceName],
+        },
+      ],
+    });
 
-    const data = await ec2.runInstances(instanceParams).promise();
+    const instanceId = data.Reservations[0].Instances[0].InstanceId;
+    console.log(`Instance ID: ${instanceId}`);
 
-    const instanceId = data.Instances[0].InstanceId;
+    // Set the command you want to run on the EC2 instance
+    const command = 'ls -l';
 
-    const commandParams = {
-      InstanceIds: [instanceId],
+    // Start the EC2 instance
+    await ec2Client.startInstances({ InstanceIds: [instanceId] });
+    console.log(`Instance started: ${instanceId}`);
+
+    // Wait for the EC2 instance to start
+    await ec2Client.waitFor('instanceRunning', { InstanceIds: [instanceId] });
+    console.log(`Instance running: ${instanceId}`);
+
+    // Send the command to the EC2 instance using SSM
+    const ssmClient = new SSMClient({ region });
+    const data2 = await ssmClient.send(new SendCommandCommand({
       DocumentName: 'AWS-RunShellScript',
-      Parameters: {
-        'commands': ['your-command-here'],
-      },
+      InstanceIds: [instanceId],
+      Parameters: { commands: [command] },
+    }));
+
+    console.log(`Command sent to instance: ${instanceId}`);
+    console.log(`Command ID: ${data2.Command.CommandId}`);
+    
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(`Command sent to instance: ${instanceId}\nCommand ID: ${data2.Command.CommandId}`),
     };
-
-    const commandData = await ec2.sendCommand(commandParams).promise();
-
-    console.log(`EC2 instance ${instanceId} started and command ${commandParams.Parameters.commands} is running`);
-  } catch (error) {
-    console.error(error);
+    return response;
+  } catch (err) {
+    console.log(`Error: ${err}`);
+    const response = {
+      statusCode: 500,
+      body: JSON.stringify(`Error: ${err}`),
+    };
+    return response;
   }
 };
